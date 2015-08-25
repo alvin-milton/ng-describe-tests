63fc2a68b11f37ec366d4eee8485bdb48f7869b0
alvin-milton <vinny.pesos@gmail.com>
2015-08-25 16:54:31 -0400

    add test

--- test/add-spec.js
+++ test/add-spec.js
@@ -17,4 +17,5 @@ describe('add', () => {
   var add;
   beforeEach(() => add = require('../add'));
   it('is a function', () => la(check.fn(add), 'expected add function', add));
+  it('adds numbers', () => la(add(2,3) === 5));
 });
