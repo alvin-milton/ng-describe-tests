b0334d8ecac088cc9eedc3cd0011ed4d89f68b2b
alvin-milton <vinny.pesos@gmail.com>
2015-08-25 16:33:30 -0400

    es6 version

--- test/add-spec.js
+++ test/add-spec.js
@@ -1,11 +1,20 @@
 require('lazy-ass');
 var check = require('check-more-types');
-describe('add', function() {
-    var add;
-    beforeEach(function() {
-        add = require('../add');
-    });
-    it('is a function', function () {
-        la(check.fn(add), 'expected add function', add);
-    });
+
+// es5
+// describe('add', function() {
+//     var add;
+//     beforeEach(function() {
+//         add = require('../add');
+//     });
+//     it('is a function', function () {
+//         la(check.fn(add), 'expected add function', add);
+//     });
+// });
+
+// es6
+describe('add', () => {
+  var add;
+  beforeEach(() => add = require('../add'));
+  it('is a function', () => la(check.fn(add), 'expected add function', add));
 });
